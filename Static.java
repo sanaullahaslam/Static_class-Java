c7d87b87fed5e67b283cb568bd27d37df7f0d0a9
Sanaullah Aslam <107422943+sanaullahaslam@users.noreply.github.com>
2023-02-21 23:45:08 +0500

    Add files via upload

---
+++ Static.java
@@ -0,0 +1,15 @@
+class Static {
+
+static int number=023210010;
+static String dept="Computer Science";
+static String name="Sanaullah";
+
+
+public static void main (String args [])
+{
+
+Static st=new Static();
+System.out.println("Name "+name+" Department "+dept+" cms "+number);
+}
+
+}
